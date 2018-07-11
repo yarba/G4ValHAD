e953d29bfbbda068bd27a1a9bcf3c0b686d8199a
Julia Yarba <yarba_j@tev.fnal.gov>
2018-07-11 12:51:06 -0500

    update batch scripts

--- test75/batch/submit_all.sh
+++ test75/batch/submit_all.sh
@@ -11,27 +11,26 @@ echo " you must specify Geant4 release/version; exit"
 exit 3
 fi
 
-#source /products/setup
 source /g4/g4p/pbs/g4-had-validation/build-scripts/g4_set_prod.sh
 
-if [ `echo ${PATH} | grep pbs` ]; then
-echo "PBS is already set"
-else
-export PATH=${PATH}:/usr/local/pbs/bin
-fi
+# ---> PBS/obsolete ---> 
+#if [ `echo ${PATH} | grep pbs` ]; then
+#echo "PBS is already set"
+#else
+#export PATH=${PATH}:/usr/local/pbs/bin
+#fi
 
 #
 # cleanup logs/err from earlier rounds
 #
 /bin/rm -f *.sh.o*
 /bin/rm -f *.sh.e*
+/bin/rm -f slurm*.out
 
 test_list=`ls -l | grep run | awk '{print $9}'`
 
 for test in ${test_list} ; do
   echo "... Processing ... ${test} ... for ${1}"
-#   qsub -q grunt -v G4RELEASE=${g4version},G4REGRESSION=${2} ${test}
-# -->   qsub -q amd32_Geant4 -l nodes=1:amd32 -v G4RELEASE=${g4version},G4REGRESSION=${2} -A g4p ${test}
-   qsub -q amd32_g4val -l nodes=1:slow,walltime=12:00:00 -v G4RELEASE=${g4version},G4REGRESSION=${2} -A g4p ${test}
-
+# ---> PBS/obsolete --->    qsub -q amd32_g4val -l nodes=1:slow,walltime=12:00:00 -v G4RELEASE=${g4version},G4REGRESSION=${2} -A g4p ${test}
+sbatch -p amd32_g4val_slow -t 12:00:00 --export="G4RELEASE=${g4version},G4REGRESSION=${2}" -A g4p ${test}
 done
